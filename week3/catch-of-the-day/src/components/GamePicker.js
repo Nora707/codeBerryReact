85181df7623b33a8588302f84c06089a3a270bb0
noramajoros0@gmail.com <Hozjin5>
2020-04-30 15:53:33 +0200

    picker js

---
+++ week3/catch-of-the-day/src/components/GamePicker.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class GamePicker extends React.Component{
+	render(){
+		return <p>Heloo</p>
+	}
+}
+ export default GamePicker;
--- week3/catch-of-the-day/src/index.js
+++ week3/catch-of-the-day/src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import GamePicker from './components/GamePicker';
+
+
+class StorePicker extends React.Component{
+	render(){
+		return <p>Heloo</p>
+	}
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StorePicker />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
