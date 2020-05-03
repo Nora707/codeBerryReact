b8452dd9d9663e38f6632870fa2f90af2cdaee08
noramajoros0@gmail.com <Hozjin5>
2020-05-03 14:11:12 +0200

    routing with push

--- week3/catch-of-the-day/src/components/StorePicker.js
+++ week3/catch-of-the-day/src/components/StorePicker.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
 
-class StorePicker extends React.Component{
-	render(){
-		return (
-			<form className="selector">
-				<h2>Please enter a name</h2>
-				<input type="text" required placeholder="enter a name"/>
-				<button type="submit">Visit store </button>
-			</form>
-		)
-	}
+class StorePicker extends React.Component {
+    myInput = React.createRef();
+    goToStore = event => {
+        event.preventDefault();
+        console.log(this.myInput.current.value);
+        this.props.history.push(`/store/${this.myInput.current.value}`)
+    };
+
+    render() {
+        return (
+            <form className="store-selector" onSubmit={this.goToStore}>
+                <h2>Please enter a name</h2>
+                <input type="text" required placeholder="enter a name" ref={this.myInput}/>
+                <button type="submit">Visit store</button>
+            </form>
+        )
+    }
 }
- export default StorePicker;
+
+export default StorePicker;
